a4ba370c2a73719ce246e3b35ee5508972d0009b
xkcm <maciejka.citko@gmail.com>
2021-12-02 14:38:42 +0100

    v1.0.1

--- dist/utilities/CacheableDecorator.d.ts
+++
@@ -1,7 +0,0 @@
-import { CacheableConfiguration } from './Types.d';
-/**
- * Cacheable decorator for methods
- * @param {CacheableConfiguration} options - Options for `Cacheable` decorator
- */
-declare function Cacheable(options?: CacheableConfiguration): (target: Object, propertyKey: string, descriptor: PropertyDescriptor) => void;
-export { Cacheable };
--- dist/utilities/MakeCacheable.d.ts
+++
@@ -1,7 +0,0 @@
-import { CacheableConfiguration, CachedFunction, CacheInstanceInfo } from "./Types.d";
-declare function makeCacheable<T extends (...args: any[]) => any>(fn: T, options?: CacheableConfiguration): CachedFunction<T>;
-declare namespace makeCacheable {
-    var tempOptions: CacheInstanceInfo;
-    var withOptions: (opts: CacheInstanceInfo) => typeof makeCacheable;
-}
-export { makeCacheable };
